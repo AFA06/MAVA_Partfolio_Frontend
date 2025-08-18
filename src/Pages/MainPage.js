3fa942a533402f1af9420c54a036dad5e6cdf314
Abdurashid <fattokhovabdurashid@gmail.com>
2025-08-18 14:32:01 +0500

    deleted unused files

--- src/Pages/MainPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function MainPage() {
-  return <div className="p-10">This is the Blog page</div>;
-}
-
-export default MainPage;
--- src/Pages/Suppliers.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Suppliers() {
-  return <div className="p-10">This is the Suppliers page</div>;
-}
-
-export default Suppliers;
