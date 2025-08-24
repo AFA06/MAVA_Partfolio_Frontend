cd53ad72e22b13a58d13e4bf740a3be847224443
Abdurashid <fattokhovabdurashid@gmail.com>
2025-08-25 00:24:20 +0500

    created in order to not repeat

---
+++ src/Components/Navbar/useNavbarHeight.js
@@ -0,0 +1,25 @@
+// src/hooks/useNavbarHeight.js
+import { useEffect, useState } from "react";
+
+export default function useNavbarHeight() {
+  const [navHeight, setNavHeight] = useState(0);
+
+  useEffect(() => {
+    const navbar = document.getElementById("navbar");
+
+    if (navbar) {
+      setNavHeight(navbar.getBoundingClientRect().height);
+    }
+
+    const handleResize = () => {
+      if (navbar) {
+        setNavHeight(navbar.getBoundingClientRect().height);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return navHeight;
+}
