83bc2a6b3fa274477d35471439766cb269c8d1d1
Abdurashid <fattokhovabdurashid@gmail.com>
2025-12-03 23:01:02 +0000

    fixed sidebar on mobile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Homepage from "./Components/Homepage/Homepage";
 import Portfolio from "./Pages/Portfolio";
-import Blog from "./Pages/Blog";
 import Videos from "./Pages/Videos/Videos";
 import CategoryVideos from "./Pages/Videos/CategoryVideos";
 import About from "./Pages/About";
@@ -28,7 +27,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="/videos" element={<Videos />} />
           <Route path="/videos/:category" element={<CategoryVideos />} />
           <Route path="/about" element={<About />} />
